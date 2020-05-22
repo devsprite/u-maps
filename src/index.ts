9f21a1670b71c4206d3c3602c4197718fc786744
Dominique <dominique.lopez@digitregroup.com>
2020-05-22 16:39:22 +0200

    WIP - Added google maps

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello');
+//import { User } from './User';
+//import { Company } from './Company';
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 7,
+  center: {
+    lat: 44,
+    lng: 5
+  },
+});
